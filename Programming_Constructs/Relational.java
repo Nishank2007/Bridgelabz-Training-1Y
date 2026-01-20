0f5058e920a20de4827776d1c0ad15f9f99e49fc
Nishank2007 <chaturvedinishank2007@gmail.com>
2026-01-20 15:26:55 +0530

    [Nishank Chaturvedi] add the programmin g constructs file

---
+++ Programming_Constructs/Relational.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Relational{
+	public static void main(String args[]){
+		Scanner sc =new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a>=18){
+			System.out.println("Allowed");
+		}
+		else{
+			System.out.println("Not allowed");
+		}
+	}	
+}
